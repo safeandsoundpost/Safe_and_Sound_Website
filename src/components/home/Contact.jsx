ce704718ad43dd564420bdfd2a0c9815d8b48c0a
Bruno Ramirez <bramirez206@hotmail.com>
2024-11-23 14:24:27 -0500

    Contact form changed to full flexbox setup.

--- src/components/home/Contact.jsx
+++ src/components/home/Contact.jsx
@@ -99,10 +99,66 @@ export default function Contact() {
             className="relative my-16 flex w-full flex-col items-center justify-center gap-10 py-28 pb-28 align-middle"
         >
             <div
-                className="relative z-20 aspect-video h-fit w-full bg-contain bg-center bg-no-repeat text-xs font-semibold text-black sm:text-lg md:text-base lg:text-xl xl:text-2xl 2xl:text-3xl"
+                className="relative z-20 flex aspect-video h-fit w-full bg-contain bg-center bg-no-repeat align-middle text-xs font-semibold text-black sm:text-lg md:text-base lg:text-xl xl:text-2xl 2xl:text-3xl"
                 style={{ backgroundImage: `url(${contactForm})` }}
             >
-                <div className="m-auto flex h-full w-[78%] flex-grow">
+                <div className="m-auto flex h-3/4 w-[87%] flex-col">
+                    <div className="flex h-full flex-col justify-end gap-2 pb-3">
+                        <div className="flex h-[46%] flex-col gap-0 md:gap-2">
+                            <div className="flex gap-0 md:gap-5 w-[94%]">
+                                <input
+                                    className="w-[43%] bg-transparent uppercase tracking-widest placeholder:text-gray-600 md:w-[50%]"
+                                    placeholder="[  N A M E  ]"
+                                    value={name}
+                                    onInput={(e) => setName(e.target.value)}
+                                />
+                                <input
+                                    className="w-[43%] bg-transparent uppercase tracking-widest placeholder:text-gray-600 md:w-[50%]"
+                                    type="email"
+                                    placeholder="[  E M A I L  ]"
+                                    value={email}
+                                    onInput={(e) => setEmail(e.target.value)}
+                                />
+                            </div>
+                            <input
+                                className="w-[85%] bg-transparent uppercase tracking-widest placeholder:text-gray-600 md:w-[88%]"
+                                type="tel"
+                                placeholder="[  P H O N E   N U M B E R  ]"
+                                value={phone}
+                                onInput={(e) => setPhone(e.target.value)}
+                            />
+                        </div>
+                    </div>
+                    <div className="flex h-full w-[90%] self-center">
+                        <div className=""></div>
+                        <div className="m-auto flex h-full w-[78%] flex-grow">
+                            <div className="m-auto h-full w-full">
+                                <textarea
+                                    className="h-full w-full resize-none bg-transparent text-[.45em] leading-[0.65rem] tracking-widest placeholder:text-gray-600 sm:text-xs md:text-xs md:leading-[1.2rem] lg:text-base lg:leading-[2.3rem] xl:text-lg"
+                                    placeholder="[ WHAT CAN WE HELP YOU WITH? ]"
+                                    value={message}
+                                    onInput={(e) => setMessage(e.target.value)}
+                                />
+                            </div>
+                            <div className="m-auto flex h-full w-[38%] justify-center">
+                                {!isSubmitted && (
+                                    <button
+                                        className="btn btn-xs z-30 h-[1em] w-[10em] p-0 self-end text-[.55em] uppercase tracking-[.2em] md:btn-xs lg:btn-sm xl:btn-md xxs:text-[.75em] sm:text-xs md:text-xs lg:text-sm xl:text-lg 2xl:text-lg"
+                                        src={contactForm}
+                                        onClick={submitForm}
+                                    >
+                                        {isLoading && (
+                                            <span className="loading loading-spinner loading-md"></span>
+                                        )}
+                                        submit
+                                    </button>
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* <div className="m-auto flex h-full w-[78%] flex-grow border-2">
                     <div className="m-auto h-[75%] w-full"></div>
                     <div className="m-auto flex h-[75%] w-[40%] justify-center">
                         {!isSubmitted && (
@@ -118,8 +174,8 @@ export default function Contact() {
                             </button>
                         )}
                     </div>
-                </div>
-                <div className="absolute left-[8%] top-[30%] flex flex-col gap-0 sm:left-[7%] md:left-[8%] md:gap-0 lg:left-[8%] lg:gap-1 xl:left-[8%] xl:top-[32%] 2xl:gap-6">
+                </div> */}
+                {/* <div className="absolute left-[8%] top-[30%] flex flex-col gap-0 sm:left-[7%] md:left-[8%] md:gap-0 lg:left-[8%] lg:gap-1 xl:left-[8%] xl:top-[32%] 2xl:gap-6">
                     <div className="flex gap-0 md:gap-5">
                         <input
                             className="w-[43%] bg-transparent uppercase tracking-widest placeholder:text-gray-600 md:w-[50%]"
@@ -142,8 +198,8 @@ export default function Contact() {
                         value={phone}
                         onInput={(e) => setPhone(e.target.value)}
                     />
-                </div>
-                <div
+                </div> */}
+                {/* <div
                     className="absolute left-[12%] top-[50%] flex h-[8em] w-[54%] flex-col gap-6 text-[.45em] 
                 sm:left-[12%] sm:top-[52%] sm:text-xs
                 md:left-[75px] md:top-[50.5%] md:text-xs
@@ -156,7 +212,7 @@ export default function Contact() {
                         value={message}
                         onInput={(e) => setMessage(e.target.value)}
                     />
-                </div>
+                </div> */}
             </div>
             <img
                 draggable="false"
