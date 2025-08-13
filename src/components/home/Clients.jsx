4eb73004fbba99a065dfaba9cd3388bb1621900f
Bruno Ramirez <bramirez206@hotmail.com>
2025-08-12 22:50:38 -0400

    Adapting Clients section to mobile resolutions.

--- src/components/home/Clients.jsx
+++ src/components/home/Clients.jsx
@@ -25,6 +25,8 @@ export default function Clients() {
     const imgRefs = useRef([]);
 
     useEffect(() => {
+        if (document.width < 768) return;
+
         const handleMouseMove = (e) => {
             imgRefs.current.forEach((img) => {
                 if (!img) return;
@@ -64,12 +66,12 @@ export default function Clients() {
     return (
         <section id="clients" className="relative m-auto flex w-full flex-col items-center justify-center gap-10 pt-5 align-middle">
             <h2 className="text-secondary w-full pt-10 text-center text-4xl font-bold tracking-widest uppercase">Clients / Partnerships</h2>
-            <div className="flex items-center justify-center gap-24 py-10">
+            <div className="flex flex-col items-center justify-center align-middle md:flex-row md:gap-24 md:py-10">
                 {client_list.map((x, i) => (
                     <figure key={i} className="tooltip tooltip-secondary toolt perspective-near before:text-xl! before:font-bold" data-tip={x.tooltip}>
                         <img
                             ref={(el) => (imgRefs.current[i] = el)}
-                            className="aspect-square w-72 object-contain object-center mix-blend-color-burn transition-transform duration-150 ease-out perspective-midrange transform-3d hover:scale-125"
+                            className="aspect-square w-40 object-contain object-center mix-blend-color-burn transition-transform duration-150 ease-out perspective-midrange transform-3d hover:scale-125 md:w-72"
                             src={`/images/clients/${x.logo}`}
                         />
                         <figcaption className="invisible">{x.caption}</figcaption>
